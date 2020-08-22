4d09c4230f9de8abe75a119a07126be6e1d6b4cb
Kabirul-Hasan-Sikder <kabir.webDev@gmail.com>
2020-08-22 22:57:30 +0600

    setting up questions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
-
-const API_URL =
-  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+import Question from "./Questions/Question";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Question />
     </div>
   );
 }
--- src/Questions/Question.js
+++ src/Questions/Question.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./Question.css";
+import { useState } from "react";
+
+const API_URL =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
 
 function Question() {
-  return <div></div>;
+  const [question, setQuestion] = useState([]);
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestion(data.results);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <span className="question_section">
+        <h2 className="question">This is the question Section.</h2>
+      </span>
+      <div>
+        <button className="option_style">Option A</button>
+        <button className="option_style">Option B</button> <br />
+        <button className="option_style">Option C</button>
+        <button className="option_style">Option D</button>
+      </div>
+    </div>
+  );
 }
 
 export default Question;

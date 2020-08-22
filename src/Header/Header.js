f9d9de66e6c57e936f71d23b93600dd9c8deeb39
Kabirul-Hasan-Sikder <kabir.webDev@gmail.com>
2020-08-22 16:56:40 +0600

    Question Pattern

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="navbar">
+      <span className="nav_text">Let's Use the Brain</span>
+    </div>
+  );
+}
+
+export default Header;

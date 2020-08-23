bc301d1994a181b2955c2580ef0e27173b89ef59
Kabirul-Hasan-Sikder <kabir.webDev@gmail.com>
2020-08-23 17:44:11 +0600

    Random answer setting

--- src/Questions/Question.js
+++ src/Questions/Question.js
@@ -11,7 +11,36 @@ function Question({
   const shuffledAnswer = [correct_answer, ...incorrect_answers].sort(
     () => Math.random() - 0.5
   );
-  return <div></div>;
+  return (
+    <div>
+      <div className="container">
+        <span className="question_section">
+          <h2
+            className="question"
+            dangerouslySetInnerHTML={{ __html: question }}
+          />
+        </span>
+        <div>
+          <Button
+            onCLick={() => handleAnswer(shuffledAnswer[0])}
+            answer={shuffledAnswer[0]}
+          />
+          <Button
+            onCLick={() => handleAnswer(shuffledAnswer[1])}
+            answer={shuffledAnswer[1]}
+          />
+          <Button
+            onCLick={() => handleAnswer(shuffledAnswer[2])}
+            answer={shuffledAnswer[2]}
+          />
+          <Button
+            onCLick={() => handleAnswer(shuffledAnswer[3])}
+            answer={shuffledAnswer[3]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Question;
